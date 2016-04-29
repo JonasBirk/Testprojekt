f30126bcad5a917c0e8aab55b7ca309b10e8a6d4
JonasBirk <jonasbirk8@gmail.com>
2016-04-29 12:30:47 +0200

    test

---
+++ test/src/testtest/client/GreetingService.java
@@ -0,0 +1,12 @@
+package testtest.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client-side stub for the RPC service. 123
+ */
+@RemoteServiceRelativePath("greet")
+public interface GreetingService extends RemoteService {
+	String greetServer(String name) throws IllegalArgumentException;
+}
